e61092195bf4bacf3fdc3e3871b055255017289b
Jonathan Taylor <jt@jonathantaylor.dev>
2021-01-30 19:33:18 +0000

    Install sqlite3 in /home/runner Directory

--- main.sh
+++ main.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "Remove existing sqlite3..."
-rm -rf sqlite3
-rm -rf sqlite-tools-linux-x86-3340100
-rm -f sqlite3.zip
+rm -rf $HOME/sqlite3
+rm -rf $HOME/sqlite-tools-linux-x86-3340100
+rm -f $HOME/sqlite3.zip
 
 echo "Download sqlite3..."
-curl https://www.sqlite.org/2021/sqlite-tools-linux-x86-3340100.zip --output sqlite3.zip
+curl https://www.sqlite.org/2021/sqlite-tools-linux-x86-3340100.zip --output $HOME/sqlite3.zip
 
 echo "Unzip download..."
-unzip sqlite3.zip
+unzip $HOME/sqlite3.zip -d $HOME
 
 echo "Rename folder..."
-mv sqlite-tools-linux-x86-3340100 sqlite3
+mv $HOME/sqlite-tools-linux-x86-3340100 $HOME/sqlite3
 
 echo "Set up sqlite3 alias..."
 alias sqlite3="$HOME/sqlite3/sqlite3"
@@ -21,6 +21,5 @@ echo "Running bash setup..."
 source ./.bashrc
 
 echo "Removing sqlite3 download..."
-rm -f sqlite3.zip
+rm -f $HOME/sqlite3.zip
 
-clear
