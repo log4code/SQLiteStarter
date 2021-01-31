b4320f05499c25a45a791bdc7ebc52d0192fd69f
Jonathan Taylor <jt@jonathantaylor.dev>
2021-01-31 20:57:20 +0000

    Fixing setup issues.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-rm -rf ./sqlite3
-
 curl https://www.sqlite.org/2021/sqlite-tools-linux-x86-3340100.zip --output ./sqlite3.zip
 
-echo "Unzip download..."
 unzip ./sqlite3.zip
 
-echo "Rename folder..."
 mv ./sqlite-tools-linux-x86-3340100 ./sqlite3
 
-echo "Removing sqlite3 download..."
 rm -f ./sqlite3.zip
 
 chmod +x ./main.sh
+
+touch test.txt
